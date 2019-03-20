013fb1477de64d00bd9ea37ae82c14c276cfae12
aeri5 <aeri5@kth.se>
2019-03-20 22:38:10 +0100

    continued work on reinforcement learning

--- one_agent/main.py
+++ one_agent/main.py
@@ -3,71 +3,124 @@ from q import *
 import random
 import time
 import sys
+import csv
+import matplotlib.pyplot as plt
 
-w = Warehouse()
+warehouseSize = [10, 10] #[coloumns, rows]
+squareDim = 50 #pixels per square
+w = Warehouse(warehouseSize, squareDim)
 w.update()
-qTable = QTable()
+qTable = QTable(warehouseSize[0]+2, warehouseSize[1]+2)
+actionsDict = {0:"up", 1:"right", 2:"down", 3:"left", 4:"stay"}
 
 try:
-	for i in range(1,100001):
+
+	dataLog = []
+	episodes = 0
+	goalsReached = 0
+	while True:
 		w.restart(0)
-		x_cord, y_cord = w.getAgentCoords(0)
-		state =[x_cord,y_cord]
-		epochs, penalty, reward = 0,0,0
-		done = False
-		while not done:  #Main loop
+		episodes += 1
+		oldState = w.getAgentCoords(0)
+
+		while True:
+			# print("state:", oldState)
 			if random.uniform(0,1)<qTable.epsilon:
 				action = random.randint(0, 4)
-				#print("random")
-				#print("state:", state)
-				#print("action_random:", action)
 			else:
-				# print(state)
-				action = np.argmax(qTable.qTable[state])
-				if action > 4:
-					action = 4
-				#print("state:", state)
-				#print("action:", action)
-				#print(qTable.qTable)
+				action = qTable.findBestAction(oldState)
 			w.moveAgent(0, action)
-			x_cord, y_cord = w.getAgentCoords(0)
-			next_state = [x_cord,y_cord]
-			reward = w.reward(0)
+			nextState = w.getAgentCoords(0)
+
 			collision, goalReached = w.collision(0)
+			reward = w.reward(0)
 
-			qTable.updateQTable(state, action, reward, next_state)
+			qTable.updateQTable(oldState, action, reward, nextState)
 
-			time.sleep(0.2)
+			if collision:
+				if goalReached:
+					outputStr = "Episode " + str(episodes) + ": Goal reached!"
+					goalsReached += 1
+				else:
+					outputStr = "Episode " + str(episodes) + ": Collided by moving " + actionsDict[action] + " at coords " + str(oldState) + "."
+				dataLog.append([str(episodes), str(goalsReached)])
+				# print(outputStr)
+				break
+
+			oldState = nextState
+
+		if episodes%10 == 0:
+			qTable.updateEpsilon()
+			print(str(qTable.epsilon))
+			print(str(episodes), "episodes with ", str(goalsReached), "goals reached")
+
+	w.mainloop()
+
+
+
+
+	# for i in range(1,100001):
+	# 	w.restart(0)
+	# 	x_cord, y_cord = w.getAgentCoords(0)
+	# 	state =[x_cord,y_cord]
+	# 	epochs, penalty, reward = 0,0,0
+	# 	done = False
+
+	# 	while not done:  #Main loop
+	# 		if random.uniform(0,1)<qTable.epsilon:
+	# 			action = random.randint(0, 4)
+	# 		else:
+	# 			# print(state)
+	# 			action = qTable.findBestAction(state)
+	# 			if action > 4:
+	# 				action = 4
+	# 			#print("state:", state)
+	# 			#print("action:", action)
+	# 			#print(qTable.qTable)
+	# 		w.moveAgent(0, action)
+	# 		x_cord, y_cord = w.getAgentCoords(0)
+	# 		next_state = [x_cord,y_cord]
+	# 		reward = w.reward(0)
+	# 		collision, goalReached = w.collision(0)
+
+	# 		qTable.updateQTable(state, action, reward, next_state)
+
+	# 		time.sleep(0.2)
 			
 
 
 
-			if reward < - 40:
-				penalty += 1
-				print("suger ju")
-				break
+	# 		if reward < - 40:
+	# 			penalty += 1
+	# 			print("suger ju")
+	# 			break
 
 
 
-			state = next_state
+	# 		state = next_state
 
-			epochs +=1
+	# 		epochs +=1
 
-	#qTable.updateEpsilon()
-			if i%100 == 0:
-				w.update()
-				print(qTable.qTable)
+	# #qTable.updateEpsilon()
+	# 		if i%100 == 0:
+	# 			w.update()
+	# 			print(qTable.qTable)
 
 
-		# 	if collision:
-		# 		w.restart(0)
-		# 		if goalReached:
-		# 			print("Goal reached")
-		# 		else:
-		# 			print("Restart")
-		# 	time.sleep(0.1)
-		# 	w.update()
-	w.mainloop()
+	# 	# 	if collision:
+	# 	# 		w.restart(0)
+	# 	# 		if goalReached:
+	# 	# 			print("Goal reached")
+	# 	# 		else:
+	# 	# 			print("Restart")
+	# 	# 	time.sleep(0.1)
+	# 	# 	w.update()
+	# w.mainloop()
 
 except KeyboardInterrupt:
+	with open('data.csv', 'w', newline='') as dataFile:
+	    writer = csv.writer(dataFile)
+	    writer.writerow("eg")
+	    writer.writerows(dataLog)
+	dataFile.close()	
 	sys.exit(0)
--- one_agent/warehouse.py
+++ one_agent/warehouse.py
@@ -1,9 +1,6 @@
 import tkinter
 from agent import *
 
-squareDim = 40
-warehouseSize = [10,10] #[maxX, maxY]
-windowSize = [squareDim*warehouseSize[0], squareDim*warehouseSize[1]] #[width, height]
 obstacleCoords = [[2,2], [3,2], [4,2], [5,2], [7,2], [7,3], [7,4], [3,4], [4,4], [5,4]] #[[x, y], [x, y], ...]
 obstacles = []
 agentCoords = [[0,9]] #[[x, y], [x, y], ...] (agent start locations)
@@ -12,46 +9,49 @@ goalCoords = [[9, 0]]
 goals = []
 
 class Warehouse(tkinter.Tk, object):
-	def __init__(self):
+	def __init__(self, warehouseSize, squareDim):
 		super(Warehouse, self).__init__()
+		self.warehouseSize = warehouseSize
+		self.squareDim = squareDim
+		self.windowSize = [self.squareDim*self.warehouseSize[0], self.squareDim*self.warehouseSize[1]] #[width, height]
 		self.title("Warehouse sim with one agent")
-		self.canvas = tkinter.Canvas(self, bg="#b7b7b7", width = windowSize[0], height = windowSize[1])
+		self.canvas = tkinter.Canvas(self, bg="#b7b7b7", width = self.windowSize[0], height = self.windowSize[1])
 		
 		#draw grid
-		for i in range(1, int(windowSize[0]/squareDim)): #vertical lines
-			self.canvas.create_line(i*squareDim, 0, i*squareDim, windowSize[1], fill="#444444")
-		for i in range(1, int(windowSize[1]/squareDim)): #horizontal lines
-			self.canvas.create_line(0, i*squareDim, windowSize[0], i*squareDim, fill="#444444")
+		for i in range(1, int(self.windowSize[0]/self.squareDim)): #vertical lines
+			self.canvas.create_line(i*self.squareDim, 0, i*self.squareDim, self.windowSize[1], fill="#444444")
+		for i in range(1, int(self.windowSize[1]/self.squareDim)): #horizontal lines
+			self.canvas.create_line(0, i*self.squareDim, self.windowSize[0], i*self.squareDim, fill="#444444")
 
 		#init shelves
 		for obstacle in obstacleCoords:
-			obstacles.append(self.canvas.create_rectangle(obstacle[0]*squareDim+1, obstacle[1]*squareDim+1, (obstacle[0]+1)*squareDim-1, (obstacle[1]+1)*squareDim-1, fill="#444444", outline="#444444"))
+			obstacles.append(self.canvas.create_rectangle(obstacle[0]*self.squareDim+1, obstacle[1]*self.squareDim+1, (obstacle[0]+1)*self.squareDim-1, (obstacle[1]+1)*self.squareDim-1, fill="#444444", outline="#444444"))
 
 		#init agent
 		agentCounter = 0
 		for agent in agentCoords:
-			agents.append(Agent(agent[0], agent[1], self.canvas.create_rectangle(agent[0]*squareDim+1, agent[1]*squareDim+1, (agent[0]+1)*squareDim-1, (agent[1]+1)*squareDim-1, fill="#eda061", outline="#eda061", tags=str(agentCounter))))
+			agents.append(Agent(agent[0], agent[1], self.canvas.create_rectangle(agent[0]*self.squareDim+1, agent[1]*self.squareDim+1, (agent[0]+1)*self.squareDim-1, (agent[1]+1)*self.squareDim-1, fill="#eda061", outline="#eda061", tags=str(agentCounter))))
 			agentCounter += 1
 
 
 		#init goal
 		for goal in goalCoords:
-			goals.append(self.canvas.create_rectangle(goal[0]*squareDim+1, goal[1]*squareDim+1, (goal[0]+1)*squareDim-1, (goal[1]+1)*squareDim-1, fill="#a8e298", outline="#a8e298"))	
+			goals.append(self.canvas.create_rectangle(goal[0]*self.squareDim+1, goal[1]*self.squareDim+1, (goal[0]+1)*self.squareDim-1, (goal[1]+1)*self.squareDim-1, fill="#a8e298", outline="#a8e298"))	
 
 		self.canvas.pack()
 
 	def moveAgent(self, index, action):
 		if action == 0:
-			self.canvas.move(agents[index].tkID, 0, -squareDim)
+			self.canvas.move(agents[index].tkID, 0, -self.squareDim)
 			agents[index].y = agents[index].y - 1
 		elif action == 1:
-			self.canvas.move(agents[index].tkID, squareDim, 0)
+			self.canvas.move(agents[index].tkID, self.squareDim, 0)
 			agents[index].x = agents[index].x + 1
 		elif action == 2:
-			self.canvas.move(agents[index].tkID, 0, squareDim)
+			self.canvas.move(agents[index].tkID, 0, self.squareDim)
 			agents[index].y = agents[index].y + 1
 		elif action == 3:
-			self.canvas.move(agents[index].tkID, -squareDim, 0)
+			self.canvas.move(agents[index].tkID, -self.squareDim, 0)
 			agents[index].x = agents[index].x - 1
 		elif action == 4:
 			self.canvas.move(agents[index].tkID, 0, 0)
@@ -62,15 +62,15 @@ class Warehouse(tkinter.Tk, object):
 	def restart(self, index):
 		agents[index].x = agentCoords[index][0]
 		agents[index].y = agentCoords[index][1]
-		self.canvas.coords(agents[index].tkID, agentCoords[index][0]*squareDim+1, agentCoords[index][1]*squareDim+1, (agentCoords[index][0]+1)*squareDim-1, (agentCoords[index][1]+1)*squareDim-1)
-		print("restart")
+		self.canvas.coords(agents[index].tkID, agentCoords[index][0]*self.squareDim+1, agentCoords[index][1]*self.squareDim+1, (agentCoords[index][0]+1)*self.squareDim-1, (agentCoords[index][1]+1)*self.squareDim-1)
+		# print("restart")
 
 	def collision(self, index):
-		if [agents[index].x, agents[index].y] in obstacleCoords or agents[index].x < 0 or agents[index].x > warehouseSize[0]-1 or agents[index].y < 0 or agents[index].y > warehouseSize[1]-1:
+		if [agents[index].x, agents[index].y] in obstacleCoords or agents[index].x < 0 or agents[index].x > self.warehouseSize[0]-1 or agents[index].y < 0 or agents[index].y > self.warehouseSize[1]-1: #Agent collided with obstacle or moved in to a wall
 			return True, False
-		elif [agents[index].x, agents[index].y] in goalCoords:
+		elif [agents[index].x, agents[index].y] in goalCoords:	#Agent reached its goal
 			return True, True
-		else:
+		else:	#No collision and goal not reached
 			return False, False
 
 	def reward(self, index):
@@ -85,5 +85,5 @@ class Warehouse(tkinter.Tk, object):
 		return reward
 
 	def getAgentCoords(self, index):
-		return agents[index].x, agents[index].y
+		return [agents[index].x, agents[index].y]
 
